7068b55578a3219b9647532e0914680877f39704
Ben Frith <bpfrith@gmail.com>
2017-02-23 15:19:24 +0000

    started building API in seeds.js

---
+++ seeds.js
@@ -0,0 +1,53 @@
+use the_dungeon;
+
+db.games.insert([
+  { 
+    title: "",
+    developers: "",
+    image: "",
+    music: "",
+    catagory: "",
+    player_perspective: "",
+    game_modes: "",
+    initial_release: "",
+    popularity: "",
+    platforms: "",
+    game_size: "",
+    description: ""
+  },
+  {
+    title: "",
+    developers: "",
+    image: "",
+    music: "",
+    catagory: "",
+    player_perspective: "",
+    game_modes: "",
+    initial_release: "",
+    popularity: "",
+    platforms: "",
+    game_size: "",
+    description: ""
+  }
+]);
+
+db.platforms.insert([
+  {
+    name: "",
+    manufacturor: "",
+    image: "",
+    logo: "",
+    release_date: "",
+    original_price: "",
+    description: ""
+  },
+  {
+    name: "",
+    manufacturor: "",
+    image: "",
+    logo: "",
+    release_date: "",
+    original_price: "",
+    description: ""
+  }
+]);
