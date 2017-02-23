c1c6e5df80b31b013f7409bbc7a813a9ae502b98
Ben Frith <bpfrith@gmail.com>
2017-02-23 16:13:25 +0000

    seeds.js for API

---
+++ seeds.js
@@ -0,0 +1,81 @@
+use the_dungeon;
+
+db.games.insert([
+  { 
+    title: "Tennis for Two",
+    developers: "William Higinbotham",
+    image: "https://www.google.co.uk/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwizpOLYxabSAhVEshQKHcyaAu4QjRwIBw&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3Ds2E9iSQfGdg&psig=AFQjCNGPXhQ1IMt6SN9cKtcXPq8Ex9fwHw&ust=1487950491795321",
+    music: "n/a",
+    catagory: "Sports",
+    player_perspective: "Oscilloscopt",
+    game_modes: "n/a",
+    initial_release: "1958",
+    popularity: "Exhibited and forgotten",
+    platforms: "Analogue computer",
+    game_size: "11kB",
+    description: ""
+  },
+  {
+    title: "Sonic the Hedgehog",
+    developers: "Saga",
+    image: "https://pbs.twimg.com/profile_images/686613263811907584/Ni3AWru_.png",
+    music: "n/a",
+    catagory: "Adventure/Racing",
+    player_perspective: "n/a",
+    game_modes: "n/a",
+    initial_release: "n/a",
+    popularity: "n/a",
+    platforms: "Saga Genesis",
+    game_size: "512kB",
+    description: ""
+  },
+  {
+    title: "",
+    developers: "",
+    image: "",
+    music: "",
+    catagory: "",
+    player_perspective: "",
+    game_modes: "",
+    initial_release: "",
+    popularity: "",
+    platforms: "",
+    game_size: "",
+    description: ""
+  },
+  {
+    title: "",
+    developers: "",
+    image: "",
+    music: "",
+    catagory: "",
+    player_perspective: "",
+    game_modes: "",
+    initial_release: "",
+    popularity: "",
+    platforms: "",
+    game_size: "",
+    description: ""
+  }
+]);
+
+db.platforms.insert([
+  {
+    name: "",
+    manufacturor: "",
+    image: "",
+    logo: "",
+    release_date: "",
+    original_price: "",
+    description: ""
+  },
+  {
+    name: "",
+    manufacturor: "",
+    image: "",
+    logo: "",
+    release_date: "",
+    original_price: "",
+    description: ""
+  }
+]);
