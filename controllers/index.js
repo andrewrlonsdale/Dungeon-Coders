add85a7d78efe0898b34353209f07204921cf658
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-02-24 11:36:17 +0000

    add logic to  gameDataRequester.js

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
-router.use('/api/questions', require('./questions'));
+router.use('/api/games', require('./games'));
 
 router.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/../client/src/views/index.html'));
