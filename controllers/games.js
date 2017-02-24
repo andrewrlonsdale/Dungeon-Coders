1f450786febc460502784ea5268c18f8ca8b1c9a
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-02-24 10:25:52 +0000

    add crud

--- controllers/games.js
+++ controllers/games.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var gameRouter = express.Router();
-
 var GamesQuery = require('../client/db/gamesQuery');
 var query = new GamesQuery();
 
@@ -10,4 +9,29 @@ gameRouter.get('/', function(req, res) {
   });
 });
 
+gameRouter.get('/:id', function(req, res){
+  query.all(function(results){
+    res.json(results[req.params.id]);
+  });
+});
+
+// gameRouter.post('/', function(req, res){
+//   var newgame = new Game(req.body);
+//   query.addgame(newgame, function(results){
+//     res.json(results);
+//   });
+// });
+
+// gameRouter.put('/:id', function(req, res){
+//   var updatedDetails = req.body;
+//   query.updategame(updatedDetails, req.params.id, function(results){
+//     res.json(results);
+//   });
+// });
+
+// gameRouter.delete('/:id', function(req, res){
+//   query.deletegame(req.params.id, function(results){
+//     res.json(results);
+//   })
+// });
 module.exports = gameRouter;
