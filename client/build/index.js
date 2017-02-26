329af5c9d60b49d46cdd28ef815cb4d358ca97d0
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-02-26 14:14:05 +0000

    fixed layout

--- client/build/index.js
+++ client/build/index.js
@@ -22,7 +22,7 @@ function makeWaves(parent, number, heightFactor, id) {
         setAttrs(wave, {
             'id': 'wave-' + i + id,
             'class': 'wave ' + waveType[rand(0, 1)],
-            'style': 'width:' + (size * 3) + 'px;height:' + (size * 3) + 'px;left:' + ((-size) + (size * i)) + 'px;margin-bottom:-' + size * heightFactor + 'px;-webkit-animation-delay:' + ((i / size) * 10) + 's;'
+            'style': 'width:' + (size * 3) + 'px;height:' + (size * 3) + 'px;left:' + ((-size) + (size * i)) + 'px;margin-bottom:-' + size * heightFactor + 'px;-animation-delay:' + ((i / size) * 10) + 's;'
         });
         target.appendChild(wave);
     }
@@ -59,7 +59,7 @@ function cloudTrees(type, parent, number, zMin, zMax) {
         setAttrs(tree, {
             'id': type + '-' + i,
             'class': type + ' ' + thisThing[rand(0, 6)],
-            'style': 'left:' + (rand(50 * zPos, targetWidth)) + 'px;-webkit-transform:translateZ(' + zPos + 'px)scale(' + scaler + ');transform:translateZ(' + zPos + 'px) scale(' + scaler + ');'
+            'style': 'left:' + (rand(50 * zPos, targetWidth)) + 'px;-transform:translateZ(' + zPos + 'px)scale(' + scaler + ');transform:translateZ(' + zPos + 'px) scale(' + scaler + ');'
         });
         target.appendChild(tree);
     }
@@ -68,3 +68,7 @@ function cloudTrees(type, parent, number, zMin, zMax) {
             cloudTrees('tree', 'forest', 400, 0, -450);
             cloudTrees('cloud', 'forest', 50, -500, -1000);
             cloudTrees('char', 'forest', 50, -600, -1100);
+
+
+
+            
