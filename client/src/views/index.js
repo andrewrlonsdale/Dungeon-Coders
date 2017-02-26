56c5cd786beb0e4e90aa4e1b8ad7dc7a8a002f2f
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-02-26 12:04:29 +0000

    add logic to index.js

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -27,3 +27,44 @@ function makeWaves(parent, number, heightFactor, id) {
         target.appendChild(wave);
     }
 } 
+
+function cloudTrees(type, parent, number, zMin, zMax) {
+    var thingType = {
+        tree: ['tree-one',
+            'tree-two',
+            'tree-three',
+            'tree-four',
+            'tree-pine',
+            'stone-one',
+            'stone-two',
+            'stone-one',
+            'stone-two'],
+        cloud: ['cloud-one',
+              'cloud-two',
+              'cloud-three',
+              'cloud-four',
+              'cloud-five'],
+        // char:  ['char-one',
+        //     'char-two',
+        //     'char-three',
+        //     'char-four']
+    };
+    for (i = 0; i < (number); i++) {
+        var thisThing = thingType[type];
+        var target = document.getElementById(parent);
+        var tree = document.createElement('div');
+        var targetWidth = target.offsetWidth;
+        var scaler = (type == 'tree') ? 1 + rand(-5, 5) / 10 : 1 + rand(-5, 5);
+        var zPos = rand(zMin, zMax);
+        setAttrs(tree, {
+            'id': type + '-' + i,
+            'class': type + ' ' + thisThing[rand(0, 6)],
+            'style': 'left:' + (rand(50 * zPos, targetWidth)) + 'px;-webkit-transform:translateZ(' + zPos + 'px)scale(' + scaler + ');transform:translateZ(' + zPos + 'px) scale(' + scaler + ');'
+        });
+        target.appendChild(tree);
+    }
+} // end cloudTrees
+
+            cloudTrees('tree', 'forest', 400, 0, -450);
+            cloudTrees('cloud', 'forest', 50, -500, -1000);
+            cloudTrees('char', 'forest', 50, -600, -1100);
