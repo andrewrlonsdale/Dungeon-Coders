932e396d6828c2a7644410bdb449293d000b05e6
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-02-27 11:43:26 +0000

    add logic to seeds

--- seeds.js
+++ seeds.js
@@ -176,7 +176,7 @@ db.games.insert([
     {  
       "console": "Atari 2600",
       "year": "1977",
-      // "img": "/images/...",
+      "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
       "hardwareSpec": "CPU: 1.19mhz, 128bytes RAM",
       "unitSales": "",
       "copy":"Another game-changer from Atari.  The iconic 2600 was the first games console to use microprocessor based hardware along with a cartridge format, which meant that games no longer had to be hard-coded into the machine itself. It's not working? Blow on it and put it back in... There we go!"
@@ -185,7 +185,7 @@ db.games.insert([
     {  
       "console": "Nintendo Entertainment System (NES)",
       "year": "1983",
-      // "img": "/images/...",
+      "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
       "hardwareSpec": "2A03 8-bit processor",
       "unitSales": "62,000,000",
       "copy":"The Daddy. The era changer.  This little beige box sat under millions of TV sets, spawning a generation of hardcore gamers.  Seminal games like 'Jumpman' (You may know him better as Mario), and Donkey Kong, and peripheral hardware like the 'zapper' light gun (Duck Hunt, anyone?) made the NES a stellar commercial success and cemented Nintendo's place at the top of the industry for years to come."
@@ -194,7 +194,7 @@ db.games.insert([
     {  
       "console": "Sega Master System",
       "year": "1985",
-      // "img": "/images/...",
+      "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
       "hardwareSpec": "4mhz, 8kb RAM",
       "unitSales": "~13,000,000",
       "copy":"Every generation has their schisms.  Were you a mod or a rocker? Punk or Prog? Blur or Oasis? For folks of a certain age, the big question was 'Sega or Nintendo??'.  The Master System by Sega was a big rival to the NES, offering an edgier stable of games to counter Nintendo's slightly more cutesy image.  Units moved from stores as quickly as Sonic the Hedgehog's trainers."
@@ -203,7 +203,7 @@ db.games.insert([
     {  
       "console": "Sony PlayStation",
       "year": "1994",
-      // "img": "/images/...",
+      "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
       "hardwareSpec": "R3000 @ 33.86mhz, 2MB RAM",
       "unitSales": "~103,000,000",
       "copy":"This is the moment when gaming truly came of age.  An exquisitely built machine, controller design that has endured for nearly a quarter-century and the introduction of game franchises that still thrill, excite and sell in the millions."
