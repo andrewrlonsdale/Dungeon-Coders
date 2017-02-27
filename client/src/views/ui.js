66c4227db861bcc33900b4a13d3c9dee9c93a101
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-02-27 23:20:43 +0000

    refactor index.js

--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -90,14 +90,14 @@ UI.prototype = {
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
 
-    // When the user ch3cks on <span> (x), close the modal
-    span.onch3ck = function() {
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
       modal.style.display = "none";
 
     }
 
-    // When the user ch3cks anywhere outside of the modal, close it
-    window.onch3ck = function(event) {
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
       if (event.target == modal) {
         modal.style.display = "none";
       }
