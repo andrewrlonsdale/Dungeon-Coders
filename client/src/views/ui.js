9b90c2af67b2769adaec89ca1100c3e1427e713b
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-02-27 22:28:52 +0000

    refactor ui

--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -84,28 +84,29 @@ UI.prototype = {
             if(pTag.getAttribute("class") === "_" + game.tag){
               pTag.style.display = "block";
             }
-         }
+          }
         }
       }
+    }
 
-      // Get the <span> element that closes the modal
-      var span = document.getElementsByClassName("close")[0];
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
 
-      // When the user clicks on <span> (x), close the modal
-      span.onclick = function() {
+    // When the user ch3cks on <span> (x), close the modal
+    span.onch3ck = function() {
+      modal.style.display = "none";
+
+    }
+
+    // When the user ch3cks anywhere outside of the modal, close it
+    window.onch3ck = function(event) {
+      if (event.target == modal) {
         modal.style.display = "none";
-
       }
-
-      // When the user clicks anywhere outside of the modal, close it
-      window.onclick = function(event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
-        }
-      }
+    }
 
 
-    }.bind(this));
+  }.bind(this));
   },
 
 };
