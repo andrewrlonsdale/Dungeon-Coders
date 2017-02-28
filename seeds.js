b921c166c17fbd3cd1b498536b35411bc45ec877
seanfealy <seanfrancisfealy@gmail.com>
2017-02-28 15:21:11 +0000

    add atari7800 image and removed duplicate

--- seeds.js
+++ seeds.js
@@ -56,43 +56,34 @@ db.games.insert([{
     "desc":"The Daddy. The era changer.  This little beige box sat under millions of TV sets, spawning a generation of hardcore gamers.  Seminal games like 'Jumpman' (You may know him better as Mario), and Donkey Kong, and peripheral hardware like the 'zapper' light gun (Duck Hunt, anyone?) made the NES a stellar commercial success and cemented Nintendo's place at the top of the industry for years to come."
   },
   {  
-    "tag": "7",
-    "console": "Sega Master System",
-    "year": "1985",
-    "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
-    "hardwareSpec": "4mhz, 8kb RAM",
-    "unitSales": 13000000,
-    "desc":"Every generation has their schisms.  Were you a mod or a rocker? Punk or Prog? Blur or Oasis? For folks of a certain age, the big question was 'Sega or Nintendo??'.  The Master System by Sega was a big rival to the NES, offering an edgier stable of games to counter Nintendo's slightly more cutesy image.  Units moved from stores as quickly as Sonic the Hedgehog's trainers."
-  },
-  {  
     "tag": "8",
     "console": "Sega SG-1000",
     "year": "1984",
-    "img": "https://upload.wikimedia.org/wikipedia/commons/7/7b/Sega-SG-1000-Console-Set.jpg",
+    "img": "./images/platforms/sg1000.png",
     "hardwareSpec": "3.58mhz",
     "unitSales": 0,
     "desc":"The older, slightly dumber brother of the Sega master system.  Build with hardware by Texas Instruments (yes, the calculator people!) This fella was launched in Japan on the same day as the Famicom, or NES.  It had no real impact on the gaming industry, but as the forerunner to the master system, it is definitely worth a mention."
   },
   {  
-    "tag": "9",
+    "tag": "7",
     "console": "Sega Master System",
     "year": "1985",
-    "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
+    "img": "./images/platforms/master_system.png",
     "hardwareSpec": "4mhz, 8kb RAM",
     "unitSales": 13000000,
     "desc":"Every generation has their schisms.  Were you a mod or a rocker? Punk or Prog? Blur or Oasis? For folks of a certain age, the big question was 'Sega or Nintendo??'.  The Master System by Sega was a big rival to the NES, offering an edgier stable of games to counter Nintendo's slightly more cutesy image.  Units moved from stores as quickly as Sonic the Hedgehog's trainers."
   },
   {  
-    "tag": "10",
+    "tag": "9",
     "console": "Atari 7800",
     "year": "1986",
-    "img": "https://upload.wikimedia.org/wikipedia/commons/e/e8/Atari-7800-Console-Set.jpg",
+    "img": "./images/platforms/atari7800",
     "hardwareSpec": "1.78mhz",
     "unitSales": 3700000,
     "desc":"The 7800 pioneered some features that gamers today will recognise.  With it's joypad controller and 'high score cartridge', a ROM memory module which could save game data, Atari had a real groundbreaker here.  It was fully back-compatible with the Atari 2600, giving an excellent stable of games.  It sold nearly 4 million units until it was discontinued in 1992."
   },
   {
-    "tag": "11",
+    "tag": "10",
     "console": "Commodore Amiga 500",
     "year": "1987",
     "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
@@ -101,7 +92,7 @@ db.games.insert([{
     "desc":"In the early 80s, Atari employee Jay Miner began working on an ambitious computer design, codenamed Lorraine, eventually setting up a new company, Amiga Inc, to explore the idea. Running a Motorola 68000 microprocessor at 7.14 MHz with 512 KB of RAM, it was monstrously powerful, and Commodore spotted its potential, buying out the IP. The Amiga went on to became a games behemoth, boasting titles like Lemmings, Cannon Fodder and Monkey Island as well as supporting the famed demo scene, where hardcore indie coders pushed the tech to its limits."
   },
   {  
-    "tag": "12",
+    "tag": "11",
     "console": "Sega Mega-Drive (US: Genesis)",
     "year": "1988",
     "img": "https://en.wikipedia.org/wiki/Sega_Genesis#/media/File:Sega-Mega-Drive-JP-Mk1-Console-Set.jpg",
@@ -110,7 +101,7 @@ db.games.insert([{
     "desc":"So we find ourselves in the heat of the console wars. The Mega-Drive's huge library of iver 900 games was a huge draw to gamers, eespecially those drawn to the  thrill of games like Night Trap and Mortal Kombat, whose content courted so much controversy that Sega created the Videogame Rating Council, predecessor to the Entertainment Software Rating Board.  We don't think that Sonic the Hedgehog cared much though."
   },
   {  
-    "tag": "13",
+    "tag": "12",
     "console": "Atari Jaguar",
     "year": "1994",
     "img": "https://en.wikipedia.org/wiki/Atari_Jaguar#/media/File:Atari-Jaguar-Console-Set.jpg",
@@ -119,7 +110,7 @@ db.games.insert([{
     "desc":"The guys at Atari may have overstreched themselves with the Jaguar.  Marketed as the first '64-bit' console while competing with the wildly popular 16-bit machines from Nintendo and Sega, the Jaguar utilised a multi chip architecture that, while capable of rendering excellent graphics and framerates, was very difficult for developers to work with, resulting in a slightly bare library of games.  This was sadly where Atari bowed out of the 'premier league' of consoles."
   },
   {  
-    "tag": "14",
+    "tag": "13",
     "console": "Sony PlayStation",
     "year": "1994",
     "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
@@ -128,7 +119,7 @@ db.games.insert([{
     "desc":"This is the moment when gaming truly came of age.  An exquisitely built machine, controller design that has endured for nearly a quarter-century and the introduction of game franchises that still thrill, excite and sell in the millions."
   },
   {  
-    "tag": "15",
+    "tag": "14",
     "console": "Nintendo 64",
     "year": "1996",
     "img": "lksdjf",
@@ -138,7 +129,7 @@ db.games.insert([{
     "desc":"What a way to end the cartridge era. Launched with the games Super Mario 64 and Pilotwings 64, the N64 was a runaway sucess and remains the 9th highest selling console of all time. It did not have the largest selection of games, but boy, what games.  Mario 64 and Zelda: Ocarina Of Time are still regarded as among the best games ever made, and GoldenEye 007 was a seminal title in the shooter genre. ed. - One of these glorious machines still has pride of place beneath my TV, 20 years on!"
   },
   {  
-    "tag": "16",
+    "tag": "15",
     "console": "Sega Dreamcast",
     "year": "1998",
     "img": "hhttps://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Dreamcast-Console-Set.png/1280px-Dreamcast-Console-Set.png",
@@ -147,7 +138,7 @@ db.games.insert([{
     "desc":"The dreamcast is one of those pieces of tech that found themselves, like the betamax and minidisc, slightly ahead of their time.  There was limited support for third party software publishers which resulted in a limited library of games, but this console featured innovations like a modem to enable online play."
   },
   {
-    "tag": "17",
+    "tag": "16",
     "console": "PlayStation 2",
     "year": "2000",
     "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
@@ -156,7 +147,7 @@ db.games.insert([{
     "desc":"The PS2 was quite simply a behemoth.  It still remains the highest-selling console of all time, with a staggering 155,000,000 units shipped to date.  Add to that the 4000 strong game library, and total of 1.5 BILLION games sold, and you have an era defining console.  Titles like the Grand Theft Auto and Metal Gear Solid franchises mean that gamers are still firing up their PS2s to this day."
   },
   {  
-    "tag": "18",
+    "tag": "17",
     "console": "Nintendo GameCube",
     "year": "2001",
     "img": "https://en.wikipedia.org/wiki/GameCube#/media/File:GameCube-Console-Set.png",
@@ -165,7 +156,7 @@ db.games.insert([{
     "desc":"With the GameCube, Nintendo release their first disc-medium console (fun fact: also the first Nintendo console not to be accompanied by a Mario title upon launch).  The main innovaation of the GameCube was the ability to connect it to a gameboy advance and use the hand held console as s secondary screen and controller. Not Nintendo's most sucessful console, shipping a relativley low 22 million, but a couple of notable Mario and Zelda titles make this console fondly remembered by  its fans."
   },
   {
-    "tag": "19",
+    "tag": "18",
     "console": "Xbox 360",
     "year": "2005",
     "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
@@ -174,7 +165,7 @@ db.games.insert([{
     "desc":"The original Xbox was a fascinating statement of intent from Microsoft, but the 360 was the company’s masterpiece. Its Xbox Live multiplayer gaming service was brilliantly implemented allowing easy online play, and the addition of meta-mechanisms such as the GamerScore and Achievement points inspired players to spend longer exploring and perfecting games. Although blighted by the infamous ‘Red Ring of Death’ fault, a range of component faults that bricked thousands of machines, the 360 was a huge success, popularising the notion of the connected console and shifting 80m units."
   },
   {
-    "tag": "20",
+    "tag": "19",
     "console": "Nintendo Wii",
     "year": "2006",
     "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
