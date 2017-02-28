030fe1647aba4b0d1961a6b9e4a14cb7c63df87d
seanfealy <seanfrancisfealy@gmail.com>
2017-02-28 20:34:38 +0000

    add battlefield1 game entry

--- seeds.js
+++ seeds.js
@@ -331,6 +331,13 @@ db.games.insert([{
     "desc":" Announced in May 2013, this is the successor to the Xbox 360 and the third console in the Xbox family, and was released in North America, Europe (in some countries), Australia, and Brazil in November 2013, and in Japan, China, and other European countries in September 2014. It is the first Xbox game console to be released in China, specifically in the Shanghai Free-Trade Zone. Microsoft marketed the device as an 'all-in-one entertainment system' The Xbox One mainly competes against consoles such as Sonys PlayStation 4 and Nintendo's Wii U as part of the eighth generation of video game consoles."
   },
   {
+    "tag": "21.1",
+    "name": "Battlefield 1",
+    "Details": "Possibly the technical zenith of the FPS. Amazing graphics and sound design take you to the trenches of WW1.",
+    "pic": "./images/platforms/battlefield1.png",
+    "vid":"https://www.youtube.com/watch?v=c7nRTF2SowQ"
+  }, 
+  {
     "tag": "22",
     "console": "PlayStation 4",
     "year": "2013",
