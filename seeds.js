d4f2b98fd6c56cb1a17bce794965965065e6a032
Ben Frith <bpfrith@gmail.com>
2017-02-28 10:24:13 +0000

    reordered seeds accourding to date

--- seeds.js
+++ seeds.js
@@ -5,6 +5,16 @@ db.games.drop();
 db.games.insert([
     {
       "tag": "1",
+      "console": "Magnavox Odyssey",
+      "year": "1972",
+      "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
+      "hardwareSpec": "40 transistors and 40 diodes",
+      "unitSales": "350,000",
+      "copy":"Designed by German-American engineer Ralph Baer, pictured here with a prototype, the Magnavox Odyssey was the first commercial home video game console. Capable of displaying just three moving objects at once, the machine came with coloured overlays to place on the screen, thereby creating various play areas. Instead of microchips, the analog architecture consisted of 40 transistors and 40 diodes, while games came on printed circuit boards. This primitive set-up was enough to create a simple Table Tennis sim that would inspire the Atari classic, Pong."
+    },
+
+    {
+      "tag": "2",
       "console": "Atari Sears Tele-Games Pong System",
       "year": "1975",
       "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
@@ -14,7 +24,7 @@ db.games.insert([
     },
 
     {  
-      "tag": "2",
+      "tag": "3",
       "console": "Atari 2600",
       "year": "1977",
       "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
@@ -24,43 +34,33 @@ db.games.insert([
     },
 
     {  
-      "tag": "3",
-      "console": "Nintendo Entertainment System (NES)",
-      "year": "1983",
-      "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
-      "hardwareSpec": "2A03 8-bit processor",
-      "unitSales": 62000000,
-      "copy":"The Daddy. The era changer.  This little beige box sat under millions of TV sets, spawning a generation of hardcore gamers.  Seminal games like 'Jumpman' (You may know him better as Mario), and Donkey Kong, and peripheral hardware like the 'zapper' light gun (Duck Hunt, anyone?) made the NES a stellar commercial success and cemented Nintendo's place at the top of the industry for years to come."
-    },
-
-    {  
       "tag": "4",
-      "console": "Sega Master System",
-      "year": "1985",
-      "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
-      "hardwareSpec": "4mhz, 8kb RAM",
-      "unitSales": 13000000,
-      "copy":"Every generation has their schisms.  Were you a mod or a rocker? Punk or Prog? Blur or Oasis? For folks of a certain age, the big question was 'Sega or Nintendo??'.  The Master System by Sega was a big rival to the NES, offering an edgier stable of games to counter Nintendo's slightly more cutesy image.  Units moved from stores as quickly as Sonic the Hedgehog's trainers."
-    },
-
-    {  
-      "tag": "5",
-      "console": "Sony PlayStation",
-      "year": "1994",
-      "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
-      "hardwareSpec": "R3000 @ 33.86mhz, 2MB RAM",
-      "unitSales": 103000000,
-      "copy":"This is the moment when gaming truly came of age.  An exquisitely built machine, controller design that has endured for nearly a quarter-century and the introduction of game franchises that still thrill, excite and sell in the millions."
-    },
-
-    {  
-      "tag": "6",
       "console": "Vectrex",
       "year": "1982",
       "img": "http://i972.photobucket.com/albums/ae206/alejandro_madera/Vectrex/DSCF3576.jpg",
       "hardwareSpec": "1.5mhz",
       "unitSales": 500000,
       "copy":"This was a unique machine.  Unlike it's peers, it came built with a vector monitor, rather than needing to be plugged into a TV.  The limitations of the monochrome monitor were gotten around by suppying the user with different coloured screen overlays! They just don't make 'em like that any more..."
+    },
+
+    {
+      "tag": "5",
+      "console": "ZX Spectrum 48k",
+      "year": "1982",
+      "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
+      "hardwareSpec": "Z80 @ 3.5Hz, 16kB/48kB/128kB memory",
+      "unitSales": "5,000,000",
+      "copy":"Named the Spectrum due to its generous palette of eight colours, Sinclair’s follow up to the ZX81 was a legend of the 8bit era. Cheaper than the BBC Micro and Commodore 64, the machine (OK, it’s a computer, but we all know everyone used it as a games console) introduced a generation to both video games and programming, kickstarting the UK games industry. Its audio-visual performance was inferior to C64, but many of the decade’s finest games (Jet Set Willy, Lords of Midnight, The Hobbit) were made for the Speccy first. Developers still speak of it with misty-eyed nostalgia."
+    },
+
+    {  
+      "tag": "6",
+      "console": "Nintendo Entertainment System (NES)",
+      "year": "1983",
+      "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
+      "hardwareSpec": "2A03 8-bit processor",
+      "unitSales": 62000000,
+      "copy":"The Daddy. The era changer.  This little beige box sat under millions of TV sets, spawning a generation of hardcore gamers.  Seminal games like 'Jumpman' (You may know him better as Mario), and Donkey Kong, and peripheral hardware like the 'zapper' light gun (Duck Hunt, anyone?) made the NES a stellar commercial success and cemented Nintendo's place at the top of the industry for years to come."
     },
 
     {  
@@ -75,6 +75,16 @@ db.games.insert([
 
     {  
       "tag": "8",
+      "console": "Sega Master System",
+      "year": "1985",
+      "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
+      "hardwareSpec": "4mhz, 8kb RAM",
+      "unitSales": 13000000,
+      "copy":"Every generation has their schisms.  Were you a mod or a rocker? Punk or Prog? Blur or Oasis? For folks of a certain age, the big question was 'Sega or Nintendo??'.  The Master System by Sega was a big rival to the NES, offering an edgier stable of games to counter Nintendo's slightly more cutesy image.  Units moved from stores as quickly as Sonic the Hedgehog's trainers."
+    },
+
+    {  
+      "tag": "9",
       "console": "Atari 7800",
       "year": "1986",
       "img": "https://upload.wikimedia.org/wikipedia/commons/e/e8/Atari-7800-Console-Set.jpg",
@@ -83,8 +93,18 @@ db.games.insert([
       "copy":"The 7800 pioneered some features that gamers today will recognise.  With it's joypad controller and 'high score cartridge', a ROM memory module which could save game data, Atari had a real groundbreaker here.  It was fully back-compatible with the Atari 2600, giving an excellent stable of games.  It sold nearly 4 million units until it was discontinued in 1992."
     },
 
+    {
+      "tag": "10",
+      "console": "Commodore Amiga 500",
+      "year": "1987",
+      "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
+      "hardwareSpec": "Motorola 68000 microprocessor at 7.14 MHz with 512 KB of RAM",
+      "unitSales": "4,850,000",
+      "copy":"In the early 80s, Atari employee Jay Miner began working on an ambitious computer design, codenamed Lorraine, eventually setting up a new company, Amiga Inc, to explore the idea. Running a Motorola 68000 microprocessor at 7.14 MHz with 512 KB of RAM, it was monstrously powerful, and Commodore spotted its potential, buying out the IP. The Amiga went on to became a games behemoth, boasting titles like Lemmings, Cannon Fodder and Monkey Island as well as supporting the famed demo scene, where hardcore indie coders pushed the tech to its limits."
+    },
+
     {  
-      "tag": "9",
+      "tag": "11",
       "console": "Sega Mega-Drive (US: Genesis)",
       "year": "1988",
       "img": "https://en.wikipedia.org/wiki/Sega_Genesis#/media/File:Sega-Mega-Drive-JP-Mk1-Console-Set.jpg",
@@ -93,34 +113,14 @@ db.games.insert([
       "copy":"So we find ourselves in the heat of the console wars. The Mega-Drive's huge library of iver 900 games was a huge draw to gamers, eespecially those drawn to the  thrill of games like Night Trap and Mortal Kombat, whose content courted so much controversy that Sega created the Videogame Rating Council, predecessor to the Entertainment Software Rating Board.  We don't think that Sonic the Hedgehog cared much though."
     },
 
-    {
-      "tag": "10",
-      "console": "Magnavox Odyssey",
-      "year": "1972",
-      "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
-      "hardwareSpec": "40 transistors and 40 diodes",
-      "unitSales": "350,000",
-      "copy":"Designed by German-American engineer Ralph Baer, pictured here with a prototype, the Magnavox Odyssey was the first commercial home video game console. Capable of displaying just three moving objects at once, the machine came with coloured overlays to place on the screen, thereby creating various play areas. Instead of microchips, the analog architecture consisted of 40 transistors and 40 diodes, while games came on printed circuit boards. This primitive set-up was enough to create a simple Table Tennis sim that would inspire the Atari classic, Pong."
-    },
-
-    {
-      "tag": "11",
-      "console": "ZX Spectrum 48k",
-      "year": "1982",
-      "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
-      "hardwareSpec": "Z80 @ 3.5Hz, 16kB/48kB/128kB memory",
-      "unitSales": "5,000,000",
-      "copy":"Named the Spectrum due to its generous palette of eight colours, Sinclair’s follow up to the ZX81 was a legend of the 8bit era. Cheaper than the BBC Micro and Commodore 64, the machine (OK, it’s a computer, but we all know everyone used it as a games console) introduced a generation to both video games and programming, kickstarting the UK games industry. Its audio-visual performance was inferior to C64, but many of the decade’s finest games (Jet Set Willy, Lords of Midnight, The Hobbit) were made for the Speccy first. Developers still speak of it with misty-eyed nostalgia."
-    },
-
-    {
+    {  
       "tag": "12",
-      "console": "Commodore Amiga 500",
-      "year": "1987",
+      "console": "Sony PlayStation",
+      "year": "1994",
       "img": "http://i758.photobucket.com/albums/xx226/focus1002/ASHPik_zpsb99fb2f7.png",
-      "hardwareSpec": "Motorola 68000 microprocessor at 7.14 MHz with 512 KB of RAM",
-      "unitSales": "4,850,000",
-      "copy":"In the early 80s, Atari employee Jay Miner began working on an ambitious computer design, codenamed Lorraine, eventually setting up a new company, Amiga Inc, to explore the idea. Running a Motorola 68000 microprocessor at 7.14 MHz with 512 KB of RAM, it was monstrously powerful, and Commodore spotted its potential, buying out the IP. The Amiga went on to became a games behemoth, boasting titles like Lemmings, Cannon Fodder and Monkey Island as well as supporting the famed demo scene, where hardcore indie coders pushed the tech to its limits."
+      "hardwareSpec": "R3000 @ 33.86mhz, 2MB RAM",
+      "unitSales": 103000000,
+      "copy":"This is the moment when gaming truly came of age.  An exquisitely built machine, controller design that has endured for nearly a quarter-century and the introduction of game franchises that still thrill, excite and sell in the millions."
     },
 
     {
