6a813837a345977135f49d68762a2780f3c0beca
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-02-28 14:23:59 +0000

    fix index.js

--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -12,6 +12,11 @@ var UI = function(){
 }
 
 UI.prototype = {
+  createText: function(text, label){
+    var p = document.createElement('p');
+    p.innerText = label + text;
+    return p;
+  },
 
 
   render: function(games){
