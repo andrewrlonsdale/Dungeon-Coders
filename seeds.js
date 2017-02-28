ed81940ca7c99d86ce7350f18769234fae2c78b6
seanfealy <seanfrancisfealy@gmail.com>
2017-02-28 16:37:14 +0000

    add PS4 with change

--- seeds.js
+++ seeds.js
@@ -201,7 +201,7 @@ db.games.insert([{
     "desc":" Announced in May 2013, this is the successor to the Xbox 360 and the third console in the Xbox family, and was released in North America, Europe (in some countries), Australia, and Brazil in November 2013, and in Japan, China, and other European countries in September 2014. It is the first Xbox game console to be released in China, specifically in the Shanghai Free-Trade Zone. Microsoft marketed the device as an 'all-in-one entertainment system' The Xbox One mainly competes against consoles such as Sonys PlayStation 4 and Nintendo's Wii U as part of the eighth generation of video game consoles."
   },
   {
-    "tag": "20",
+    "tag": "22",
     "console": "PlayStation 4",
     "year": "2013",
     "img": "./images/platforms/ps4.png",
